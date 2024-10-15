2f5779ef178d5a7bf7f3a4d77bc848278d359e14
it22242204 <128572527+it22242204@users.noreply.github.com>
2024-10-15 14:07:16 +0530

    backend

---
+++ Backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./Config/db.js");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+
+//Link Routs
+const EmployeeRoute = require("./Routes/EmployeManegmentRoutes.js");
+const InventoryRoute = require("./Routes/InventroyManegmentRoutes.js");
+const RatingRoute = require("./Routes/RatingSystemRoutes.js");
+const SupplierRoute = require("./Routes/SupplyManegmentRoutes.js");
+const ProductRoute = require("./Routes/ProductManagemnetRoute.js");
+const DeliveryDrive = require("./Routes/DiliveryManegmentDriveRoutes.js");
+const VehicalDrive = require("./Routes/DiliveryManegmentVehicalRoutes.js");
+const CartRoute = require("./Routes/CartManagementRoute.js");
+const UserLoginRoute = require("./Routes/UserLoginRoute.js");
+const UserRoute = require("./Routes/UserManagementRoutes.js");
+const UserProfileRoute = require("./Routes/UserProfileRoute.js");
+const DeliveryRoute = require("./Routes/DeliveryRoute.js");
+const InformRoute = require("./Routes/informSupplyRoute.js");
+const PaymentRoute = require("./Routes/PaymentRoute.js");
+
+dotenv.config();
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/employee", EmployeeRoute);
+app.use("/inventory", InventoryRoute);
+app.use("/rates", RatingRoute);
+app.use("/supplier", SupplierRoute);
+app.use("/products", ProductRoute );
+app.use("/drive", DeliveryDrive );
+app.use("/vehical", VehicalDrive );
+app.use("/carts", CartRoute);
+app.use("/user", UserRoute);
+app.use("/login", UserLoginRoute);
+app.use("/profile", UserProfileRoute);
+app.use("/deliveri", DeliveryRoute);
+app.use("/payments", PaymentRoute);
+app.use("/inform", InformRoute);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
